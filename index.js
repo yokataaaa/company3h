2c8f07782fce3b7dfe6341fa46248fbf6d71991b
jehyuk <dlawpgur84@gmail.com>
2021-12-10 14:46:54 +0900

    update footer, portfolio-details

--- index.js
+++ index.js
@@ -6,19 +6,13 @@ portfolioContainer.addEventListener('click', (e) => {
   if (e.target.dataset.id === 'solar') {
     let detailIframeSrc = 'portfolio-details.html';
     detailIframe.setAttribute('src', detailIframeSrc);
-    console.log(e.target.dataset.id);
-    console.log(detailIframe);
   }
   else if (e.target.dataset.id === 'equipment') {
     let detailIframeSrc = 'portfolio-details1.html';
     detailIframe.setAttribute('src', detailIframeSrc);
-    console.log(e.target.dataset.id);
-    console.log(detailIframe);
   }
   else if (e.target.dataset.id === 'customize') {
     let detailIframeSrc = 'portfolio-details2.html';
     detailIframe.setAttribute('src', detailIframeSrc);
-    console.log(e.target.dataset.id);
-    console.log(detailIframe);
   }
 });
