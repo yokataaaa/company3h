1f893eb5a559f3cdd0645bb34c4c6c0a0a2ac850
jehyuk <dlawpgur84@gmail.com>
2021-12-11 13:12:41 +0900

    email update

---
+++ email.js
@@ -0,0 +1,25 @@
+// import * as functionEmail from "https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js";
+
+// functionEmail.emailjs.init("user_dzaLDc1Vii1NZ3XOpftMo");
+
+const emailSubmit = document.querySelector('#email-submit');
+
+emailSubmit.addEventListener('click', (e) => {
+
+  let templateParams = {
+    name: document.querySelector('#name').value,
+    email: document.querySelector('#email').value,
+    subject: document.querySelector('#subject').value,
+    message: document.querySelector('#email-message').value,
+  };
+
+  console.log(templateParams);
+  // functionEmail.emailjs.send('service_zv69q4e', 'template_9s2qvvt', templateParams)
+  //        //emailjs.send('service ID', 'template ID', 보낼 내용이 담긴 객체)
+  //   .then(function(response) {
+  //       console.log('SUCCESS!', response.status, response.text);
+  //   }, function(error) {
+  //       console.log('FAILED...', error);
+  //   });  
+});
+
